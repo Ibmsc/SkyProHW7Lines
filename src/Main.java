a9bdc47aa4b3b7f78a8305b3326453255dc1a276
ibmsc <Ibm.sc1409@gmail.com>
2022-06-25 22:01:32 +0300

    Коммит 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("HomeWork7 Lines");
+        System.out.println("Задача №1!");
+        /* Бухгалтерия попросила написать программу, в которой можно работать с Ф.И.О. сотрудников.
+        первая с именем firstName — для хранения имени;
+        вторая с именем middleName — для хранения отчества;
+        третья с именем lastName — для хранения фамилии;
+        четвертая с именем fullName — для хранения Ф.И.О. сотрудника в формате "Фамилия Имя Отчество".
+        Выведите в консоль фразу: “ФИО сотрудника — ….”
+        В качестве данных для задачи используйте “Ivanov Ivan Ivanovich”.
+       */
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("Задача №2!");
+        /*Напишите программу, которая изменит написание Ф. И. О. сотрудника с
+        “Ivanov Ivan Ivanovich” на полностью заглавные буквы.
+         В качестве строки с исходными данными используйте строку fullName.
+         Результат программы выведите в консоль в формате: ”Данные ФИО сотрудника для заполнения отчета — …”
+        */
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
 
 
     }
